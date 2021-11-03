fa6b6a22537fe39ab363f3da308e9ffc5743d405
konstantinklir <konstantin-klir@outlook.com>
2021-11-03 00:03:22 -0400

    Added test button to dashboard with callback function

--- index.py
+++ index.py
@@ -1,31 +1,46 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(children=[
-    html.H1(children='My Dash'),
+    html.H1(children='Smart Home Security'),
 
     html.Div(children='''
-        Dash: This is my new basic Dash.
+        Dashboard for Smart Security System.
     '''),
 
+    html.Button("ON", id="on-btn"),    
+
+    html.Div(id="output-div"),
+
     dcc.Graph(
-        id='new-graph',
+        id='temp-graph',
         figure={
             'data': [
                 {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'line', 'name': 'test1'},
            
             ],
             'layout': {
-                'title': 'New Basic Data Visualization'
+                'title': 'Temperature Recording'
             }
         }
     )
 ])
+
+@app.callback(
+    [Output("output-div", "children")],
+    [Input("on-btn", "n_clicks")]
+)
+def on_Clicked(value):
+    if value == None:
+        raise PreventUpdate
+    return [f"You clicked {value} times"]
 
 if __name__ == '__main__':
     app.run_server(debug=True)

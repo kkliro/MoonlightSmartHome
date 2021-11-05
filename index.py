e5ae82857d3fde23b52dfba97709d015e68e7914
konstantinklir <konstantin-klir@outlook.com>
2021-11-05 02:04:17 -0400

    Added humidity gage to dashboard

--- index.py
+++ index.py
@@ -12,7 +12,7 @@ import time
 import board
 import adafruit_dht
 
-dhtDevice = adafruit_dht.DHT11(board.D19)
+dhtDevice = adafruit_dht.DHT11(board.D25)
 
 GPIO.setwarnings(False)
 
@@ -23,17 +23,39 @@ GPIO.setup(LED, GPIO.OUT)
 lastTemperature = 0
 currentTemp = 0
 
+currentHum = 0
+lastHum = 0
+
 def ConfigureLED(ledStatus):
     GPIO.output(LED, ledStatus)  
         
 def GetTemp():
     try:
+        global currentTemp
+        global lastTemperature
         # Print the values to the serial port
-        currentTemp = lastTemperature
+        lastTemperature = currentTemp
         temperature_c = dhtDevice.temperature
         currentTemp = temperature_c
         #DisplayTemp()
-        print(currentTemp)
+        #print(currentTemp)
+        #temperature_f = temperature_c * (9 / 5) + 32
+        #humidity = dhtDevice.humidity
+        #print("Temp: {:.1f} F / {:.1f} C    Humidity: {}% "
+        #.format(temperature_f, temperature_c, humidity))
+    except RuntimeError as error:     # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
+        
+def GetHumidity():
+    try:
+        global currentHum
+        global lastHum
+        # Print the values to the serial port
+        lastHum = currentHum
+        humidity = dhtDevice.humidity
+        currentHum = humidity
+        #DisplayTemp()
+        #print(currentTemp)
         #temperature_f = temperature_c * (9 / 5) + 32
         #humidity = dhtDevice.humidity
         #print("Temp: {:.1f} F / {:.1f} C    Humidity: {}% "
@@ -42,7 +64,6 @@ def GetTemp():
         print(error.args[0])
 
 app = dash.Dash()
-
 
 app.layout = html.Div([
     html.H1(children='Smart Home Security'),
@@ -56,10 +77,17 @@ app.layout = html.Div([
     html.Div(id="output-div"),
     
     dcc.Graph(id='gauge-temp'),
+    dcc.Graph(id='gauge-hum'),
     
     dcc.Interval(
         id='temp-interval',
-        interval=1*5000,
+        interval=1*1000,
+        n_intervals=0
+    ),
+    
+    dcc.Interval(
+        id='hum-interval',
+        interval=1*1000,
         n_intervals=0
     )
 
@@ -69,8 +97,9 @@ app.layout = html.Div([
     Output('gauge-temp', 'figure'),
     [Input('temp-interval', 'n_intervals')]
 )
-def update_graph_live(n):
+def update_graph_live_temp(n):
     GetTemp()
+    #print(currentTemp)
     fig = go.Figure(go.Indicator(
         mode = "gauge+number+delta",
         value = currentTemp,
@@ -93,6 +122,36 @@ def update_graph_live(n):
     fig.update_layout(paper_bgcolor = "lavender", font = {'color': "darkblue", 'family': "Arial"})
     
     return fig
+
+@app.callback(
+    Output('gauge-hum', 'figure'),
+    [Input('hum-interval', 'n_intervals')]
+)
+def update_graph_live_hum(n):
+    GetHumidity()
+    #print(currentTemp)
+    fig = go.Figure(go.Indicator(
+        mode = "gauge+number+delta",
+        value = currentHum,
+        domain = {'x': [0, 1], 'y': [0, 1]},
+        title = {'text': "Humidity",'font': {'size': 24}},
+        delta = {'reference': lastHum, 'increasing': {'color': "RebeccaPurple"} },
+        gauge = {'axis': {'range': [0, 100], 'tickwidth': 1, 'tickcolor': "darkblue"},
+                'bar': {'color': "darkblue"},
+                'bgcolor': "red",
+                'borderwidth': 2,
+                'bordercolor': "gray",
+                 'steps': [
+                {'range': [0, 33], 'color': 'green'},
+                {'range': [33, 66], 'color': 'yellow'}],
+                'threshold': {
+                'line': {'color': "white", 'width': 4},
+                'thickness': 0.75,
+                'value': 99}}))
+    
+    fig.update_layout(paper_bgcolor = "beige", font = {'color': "darkblue", 'family': "Arial"})
+    
+    return fig
                 
 
 @app.callback(
@@ -105,7 +164,7 @@ def on_Clicked(value):
     
     if value == None:
         #raise PreventUpdate
-        ConfigureLED(0)
+        ConfigureLED(circuitPass)
         return [f"Light State: {lightOn}"]
     if value % 2 == 1:
         lightOn = "On"

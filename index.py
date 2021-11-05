7baa0483d15e77286ebfb39ec9fb44aaf03c0b0c
konstantinklir <konstantin-klir@outlook.com>
2021-11-05 00:37:31 -0400

    Added temperature reader to dashboard

--- index.py
+++ index.py
@@ -8,31 +8,63 @@ from dash.exceptions import PreventUpdate
 
 import RPi.GPIO as GPIO
 
+import time
+import board
+import adafruit_dht
+
+dhtDevice = adafruit_dht.DHT11(board.D24)
+
 GPIO.setwarnings(False)
 
 GPIO.setmode(GPIO.BCM)
 LED = 23
 GPIO.setup(LED, GPIO.OUT)
 
-app = dash.Dash()
-fig = go.Figure(go.Indicator(
+lastTemperature = 0
+currentTemp = 0
+
+def ConfigureLED(ledStatus):
+    GPIO.output(LED, ledStatus)
+    
+def GetTemp():
+    while True:
+        try:
+             # Print the values to the serial port
+             temperature_c = dhtDevice.temperature
+             currentTemp = temperature_c
+             DisplayTemp()
+             print(currentTemp)
+             #temperature_f = temperature_c * (9 / 5) + 32
+             #humidity = dhtDevice.humidity
+             #print("Temp: {:.1f} F / {:.1f} C    Humidity: {}% "
+                   #.format(temperature_f, temperature_c, humidity))
+        except RuntimeError as error:     # Errors happen fairly often, DHT's are hard to read, just keep going
+             print(error.args[0])
+        time.sleep(2.0)
+    
+
+def DisplayTemp():
+    return go.Indicator(
     mode = "gauge+number+delta",
-    value = 6,
+    value = currentTemp,
     domain = {'x': [0, 1], 'y': [0, 1]},
-    title = {'text': "Pressure",'font': {'size': 24}},
-    delta = {'reference': 380, 'increasing': {'color': "RebeccaPurple"} },
-    gauge = {'axis': {'range': [None, 500], 'tickwidth': 1, 'tickcolor': "darkblue"},
+    title = {'text': "Temperature",'font': {'size': 24}},
+    delta = {'reference': lastTemperature, 'increasing': {'color': "RebeccaPurple"} },
+    gauge = {'axis': {'range': [-30, 30], 'tickwidth': 1, 'tickcolor': "darkblue"},
             'bar': {'color': "darkblue"},
-            'bgcolor': "white",
+            'bgcolor': "red",
             'borderwidth': 2,
             'bordercolor': "gray",
              'steps': [
-            {'range': [0, 250], 'color': 'cyan'},
-            {'range': [250, 400], 'color': 'royalblue'}],
+            {'range': [-30, -10], 'color': 'cyan'},
+            {'range': [-10, 10], 'color': 'royalblue'}],
             'threshold': {
             'line': {'color': "red", 'width': 4},
             'thickness': 0.75,
-            'value': 490}}))
+            'value': 490}})
+
+app = dash.Dash()
+fig = go.Figure(DisplayTemp())
 
 fig.update_layout(paper_bgcolor = "lavender", font = {'color': "darkblue", 'family': "Arial"})
 
@@ -67,8 +99,7 @@ def on_Clicked(value):
         circuitPass = 1
     ConfigureLED(circuitPass)
     return [f"Light State: {lightOn}"]
+    
 
-def ConfigureLED(ledStatus):
-    GPIO.output(LED, ledStatus)    
 
 app.run_server(debug=True)
